b437f92f0121457bc3e3b117dda461b6ec5e59f3
enesdogan41 <e.dogan.bjk.ed@gmail.com>
2020-08-05 17:46:12 +0300

    created two functions

--- tttgame.py
+++ tttgame.py
@@ -1,8 +1,22 @@
 def display_board(board):
-
     print(board[7]+'|'+board[8]+'|'+board[9])
+    print("-----")
     print(board[4]+'|'+board[5]+'|'+board[6])
+    print("-----")
     print(board[1]+'|'+board[2]+'|'+board[3])
 
-test_board = ['#','X','O','X','O','X','O','X','O','x']
-display_board(test_board)
+def player_input():
+    marker = ''
+    while marker != "X" and marker != "O":
+        marker = input("Player1 choose X or O: ")
+        if marker != "X" and marker !="O" :
+            print("WRONG INPUT.YOU CAN ONLY CHOOSE X OR O !")
+    
+        player1 = marker
+
+        if player1 == 'X':
+            player2 = 'O'
+        else:
+            player2 = "X"
+    print(player1,player2)
+
