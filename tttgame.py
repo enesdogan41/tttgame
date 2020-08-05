7784a7e7399ae817e9bbb3f2e1b02ddd6f3be339
enesdogan41 <e.dogan.bjk.ed@gmail.com>
2020-08-05 17:23:10 +0300

    created display function and tested

---
+++ tttgame.py
@@ -0,0 +1,8 @@
+def display_board(board):
+
+    print(board[7]+'|'+board[8]+'|'+board[9])
+    print(board[4]+'|'+board[5]+'|'+board[6])
+    print(board[1]+'|'+board[2]+'|'+board[3])
+
+test_board = ['#','X','O','X','O','X','O','X','O','x']
+display_board(test_board)
